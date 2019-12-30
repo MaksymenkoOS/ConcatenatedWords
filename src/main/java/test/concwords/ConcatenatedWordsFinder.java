15c4dd4cc6e440a15c81dc6bbaaf1c42d3f5f6d9
sandromax <sandrondk@gmail.com>
2019-12-30 10:07:40 +0200

    refactoring ConcatenatedWordsFinder class

--- src/main/java/test/concwords/ConcatenatedWordsFinder.java
+++ src/main/java/test/concwords/ConcatenatedWordsFinder.java
@@ -1,7 +1,6 @@
 package test.concwords;
 
 import java.io.IOException;
-import java.nio.file.Path;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.LinkedList;
@@ -10,55 +9,22 @@ import java.util.stream.Collectors;
 
 public class ConcatenatedWordsFinder {
 
-    private Map<String, Integer> words = new LinkedHashMap<>();
     private LinkedList<String> concatenatedWords = new LinkedList<>();
 
-    public boolean downloadWordsFromFile(Path path) {
+    public void findConcatenatedWords() {
 
-        LinkedList<String> wordsFromFile;
-        Map<String, Integer> unsortedMap = new HashMap<>();
-
-        try {
-            wordsFromFile = FileWorker.read();
-        } catch (IOException e) {
-            e.printStackTrace();
-            return false;
-        }
-
-        for (String string : wordsFromFile) {
-
-            unsortedMap.put(string, string.length());
-
-        }
-
-        words = sortByValueReversed(unsortedMap);
-
-        if(wordsFromFile != null) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    public LinkedList<String> findConcatenatedWords() {
-
-        if(words == null) {
-            return null;
-        }
+        Map<String, Integer> words = downloadWordsFromFile();
 
         String word;
 
+        assert words != null;
         for(Map.Entry entry : words.entrySet()) {
             word = entry.getKey().toString();
             if(isWordConcatenated(word, words)) {
                 concatenatedWords.add(word);
-//                FileIO.writeConc(word + " - " + word.length());
-            } else {
-                continue;
             }
         }
 
-        return concatenatedWords;
     }
 
     public String getLongestConcatenatedWord() {
@@ -73,23 +39,41 @@ public class ConcatenatedWordsFinder {
         return concatenatedWords.size();
     }
 
-    private static Map<String, Integer> sortByValueReversed(final Map<String, Integer> wordCounts) {
+    private Map<String, Integer> downloadWordsFromFile() {
+
+        LinkedList<String> wordsFromFile;
+        Map<String, Integer> unsortedMap = new HashMap<>();
+
+        try {
+            wordsFromFile = FileWorker.read();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+
+        for (String string : wordsFromFile) {
+            unsortedMap.put(string, string.length());
+        }
+
+        return sortByValueReversed(unsortedMap);
+    }
+
+    private Map<String, Integer> sortByValueReversed(Map<String, Integer> wordCounts) {
         return wordCounts.entrySet()
                 .stream()
                 .sorted((Map.Entry.<String, Integer>comparingByValue().reversed()))
                 .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e1, LinkedHashMap::new));
     }
 
-    private static Boolean isWordConcatenated(String word, Map<String, Integer> map) {
-        String result = "";
+    private Boolean isWordConcatenated(String word, Map<String, Integer> map) {
+
+        StringBuilder result = new StringBuilder();
         int repeated = 0;
 
         for(int s = 0, e = 0; e <= word.length(); e++) {
 
-            if(!map.containsKey(word.substring(s, e))) {
-                continue;
-            } else {
-                result = result + word.substring(s, e);
+            if(map.containsKey(word.substring(s, e))) {
+                result.append(word, s, e);
 
                 repeated++;
                 s = e;
@@ -98,12 +82,8 @@ public class ConcatenatedWordsFinder {
 
         }
 
+        return word.equals(result.toString()) && repeated > 1;
 
-        if(word.equals(result) && repeated > 1) {
-            return true;
-        } else {
-            return false;
-        }
     }
 
 }
